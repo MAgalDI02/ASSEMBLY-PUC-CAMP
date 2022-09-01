b738633471985227542973b31255b91607d1ffba
MAgalDI02 <102761482+MAgalDI02@users.noreply.github.com>
2022-09-01 14:12:54 -0300

    Update lab04.asm

--- lab04.asm
+++ lab04.asm
@@ -1,63 +1,63 @@
-.model small
-.data
-.code
-    main proc
+.MODEL SMALL
+.DATA
+.CODE
+    MAIN PROC
 ;ALFABETO DE A Z
 ;PART 1 LETRAS DE A ATE J
-        MOV CL, 0       ;Contador começa em 0
-        mov dl, 97      ;DL recebe o caracter a  
-        mov ah, 02      ;Ah recebe a função 02
-        VOLTA:          ;Loop que printa de a ate j
-            int 21h     ;Chama a função que Printa o que está em DL
-            mov bl, dl  ;Bl guarda o valor de DL
-            mov dl, 10  ;DL recebe enter
-            int 21h     ;Printa o que está DL    
-            mov dl, bl  ;Dl recebe seu valor de volta
-            add dl, 1   ;Dl soma 1
-            add bl, 1   ;BL soma 1
-            INC CL      ;Funcao para soma de +1 como um i++ em C
-            CMP CL, 10  ;Compara Cl até for igual a 10
-            JNZ VOLTA   ;Caso não Cl não seja igual a 10 faz a função volta
-        mov ah, 07      ;Ah recebe a função 07
-        int 21h         ;Chama a função que le um caracter, nesse caso com a intenção de deixar deixar o código visualmente melhor
-        mov dl, bl      ;DL recebe seu valor de volta
+        MOV CL, 0       ;CONTADOR COMEÇA EM 0
+        MOV DL, 97      ;DL RECEBE O CARACTER A  
+        MOV AH, 02      ;AH RECEBE A FUNÇÃO 02
+        VOLTA:          ;LOOP QUE PRINTA DE A ATE J
+            INT 21H     ;CHAMA A FUNÇÃO QUE PRINTA O QUE ESTÁ EM DL
+            MOV BL, DL  ;BL GUARDA O VALOR DE DL
+            MOV DL, 10  ;DL RECEBE ENTER
+            INT 21H     ;PRINTA O QUE ESTÁ DL    
+            MOV DL, BL  ;DL RECEBE SEU VALOR DE VOLTA
+            ADD DL, 1   ;DL SOMA 1
+            ADD BL, 1   ;BL SOMA 1
+            INC CL      ;FUNCAO PARA SOMA DE +1 COMO UM I++ EM C
+            CMP CL, 10  ;COMPARA CL ATÉ FOR IGUAL A 10
+            JNZ VOLTA   ;CASO NÃO CL NÃO SEJA IGUAL A 10 FAZ A FUNÇÃO VOLTA
+        MOV AH, 07      ;AH RECEBE A FUNÇÃO 07
+        INT 21H         ;CHAMA A FUNÇÃO QUE LE UM CARACTER, NESSE CASO COM A INTENÇÃO DE DEIXAR DEIXAR O CÓDIGO VISUALMENTE MELHOR
+        MOV DL, BL      ;DL RECEBE SEU VALOR DE VOLTA
         
         
 ;PART 2 LETRAS DE K ATE T        
-        mov ah, 02      ;Ah recebe a função 02
-        mov cl, 0       ;Reseta o contador
-        RESET:         ;Repete um loop parecido com VOLTA, printando as letras de k a t
-            int 21h
-            mov bl, dl
-            mov dl, 10
-            int 21h
-            mov dl, bl
-            add dl, 1
-            add bl, 1
+        MOV AH, 02      ;AH RECEBE A FUNÇÃO 02
+        MOV CL, 0       ;RESETA O CONTADOR
+        RESET:         ;REPETE UM LOOP PARECIDO COM VOLTA, PRINTANDO AS LETRAS DE K A T
+            INT 21H
+            MOV BL, DL
+            MOV DL, 10
+            INT 21H
+            MOV DL, BL
+            ADD DL, 1
+            ADD BL, 1
             INC CL
             CMP CL, 10 
             JNZ RESET
-         mov ah, 07
-         int 21h
-         mov dl, bl
+         MOV AH, 07
+         INT 21H
+         MOV DL, BL
          
 ;PART 3 LETRAS DE U ATE Z         
-         mov ah, 02
-         mov cl, 0
-         LOP:         ;Repete um loop parecido com VOLTA, printando as letras de u a z
-            int 21h
-            mov bl, dl
-            mov dl, 10
-            int 21h
-            mov dl, bl
-            add dl, 1
-            add bl, 1
+         MOV AH, 02
+         MOV CL, 0
+         LOP:         ;REPETE UM LOOP PARECIDO COM VOLTA, PRINTANDO AS LETRAS DE U A Z
+            INT 21H
+            MOV BL, DL
+            MOV DL, 10
+            INT 21H
+            MOV DL, BL
+            ADD DL, 1
+            ADD BL, 1
             INC CL
             CMP CL, 6
             JNZ LOP
-         mov ah, 07
-         int 21h
-         mov ah, 02
+         MOV AH, 07
+         INT 21H
+         MOV AH, 02
          
          MOV AH,02    
          MOV CL,0 
@@ -110,25 +110,25 @@
          INT 21H
         
  ;NUMEROS DE 0 A 9           
-        mov cl, 0
-        mov dl, 48
+        MOV AH,02
+        MOV CL,0
+        MOV DL,48
         
-        REST:      ;Repete um loop parecido como o VOLTA, mas dessa vez printando os numeros de 0 a 9
-            int 21h
-            mov bl, dl
-            mov dl, 10
-            int 21h
-            mov dl, bl
-            add dl, 1
+        REST:      ;REPETE UM LOOP PARECIDO COMO O VOLTA, MAS DESSA VEZ PRINTANDO OS NUMEROS DE 0 A 9
+            INT 21H
+            MOV BL, DL
+            MOV DL, 10
+            INT 21H
+            MOV DL, BL
+            ADD DL, 1
             INC CL
             CMP CL, 10
             JNZ REST
-        mov ah, 4Ch
-        int 21h
+        MOV AH,07
+        MOV AH,04CH
+        INT 21H
         
         
         
-    main endp
-end main
- 
-    
+    MAIN ENDP
+END MAIN
